8e3bf62d36a1f22dd4e49709b2d141d3eae58a4b
Nikola <djnik8225@gmail.com>
2019-01-08 03:38:14 +0100

    Updated register form in react and minor bug fixed

--- validation/register.js
+++ validation/register.js
@@ -1,52 +1,51 @@
-const Validator = require('validator');
-const isEmpty = require('./is-empty'); 
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
 
-module.exports = function validateRegisterInput(data){
-    let errors = {};
+module.exports = function validateRegisterInput(data) {
+  let errors = {};
 
-    data.name = !isEmpty(data.name) ? data.name : "";
-    data.email = !isEmpty(data.email) ? data.email : "";
-    data.password = !isEmpty(data.password) ? data.password : "";
-    data.password2 = !isEmpty(data.password2) ? data.password2 : "";
+  data.name = !isEmpty(data.name) ? data.name : "";
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+  data.password2 = !isEmpty(data.password2) ? data.password2 : "";
 
-    // Checking for name and password length
-    if( !Validator.isLength(data.name, { min: 2, max: 30}) ){
-        errors.name = "Name must be between 2 and 30 characters";
-    }
+  // Checking for name and password length
+  if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
+    errors.name = "Name must be between 2 and 30 characters";
+  }
 
-    if( !Validator.isLength(data.password, { min: 6, max: 30}) ){
-        errors.password = "Password must be between 6 and 30 characters";
-    }
+  if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+    errors.password = "Password must be between 6 and 30 characters";
+  }
 
-    // Checking out if input is an empty string and if email is valid
-    if( Validator.isEmpty(data.name)){
-        errors.name = "Name field is required";
-    }
+  // Checking out if input is an empty string and if email is valid
+  if (Validator.isEmpty(data.name)) {
+    errors.name = "Name field is required";
+  }
 
-    if( Validator.isEmpty(data.email)){
-        errors.name = "Email field is required";
-    }
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email field is required";
+  }
 
-    if( !Validator.isEmail(data.email)){
-        errors.email = "Email is invalid";
-    }
-    
-    if( Validator.isEmpty(data.password)){
-        errors.password = "Password field is required";
-    }
-    
-    if( Validator.isEmpty(data.password2)){
-        errors.password2 = "Confirm password field is required";
-    }
+  if (!Validator.isEmail(data.email)) {
+    errors.email = "Email is invalid";
+  }
 
-    // Checking are 2 passwords same
-    if( !Validator.equals(data.password, data.password2)){
-        errors.password2 = "Passwords must match";
-    }
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password field is required";
+  }
 
-    return {
-        errors,
-        isValid: isEmpty(errors)
-    }
+  if (Validator.isEmpty(data.password2)) {
+    errors.password2 = "Confirm password field is required";
+  }
 
+  // Checking are 2 passwords same
+  if (!Validator.equals(data.password, data.password2)) {
+    errors.password2 = "Passwords must match";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
 };
