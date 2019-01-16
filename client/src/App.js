41e7c22a507103301908dfc2cc5139a17f3c10d7
cveta96 <djnik8225@gmail.com>
2019-01-16 21:22:07 +0100

    Have to fix the header redirect links, some catch warrning with backend api?

--- client/src/App.js
+++ client/src/App.js
@@ -25,6 +25,10 @@ import EditProfile from "./components/edit-profile/EditProfile";
 import AddExperience from "./components/add-credentials/AddExperience";
 import AddEducation from "./components/add-credentials/AddEducation";
 
+// Profiles and comments
+import Profiles from "./components/profiles/Profiles";
+import Profile from "./components/profile/Profile";
+
 import "./App.css";
 
 // Check for token
@@ -57,6 +61,8 @@ class App extends Component {
             <Route exact path="/" component={Landing} />
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
+            <Route exact path="/profiles" component={Profiles} />
+            <Route exact path="/profile/:handle" component={Profile} />
             <Switch>
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
             </Switch>
